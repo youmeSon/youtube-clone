7de8ecbe870ab706dfccf51e1d6f267f0f20d954
Youme <winellyoume@gmail.com>
2021-03-28 22:20:43 +0100

    Deconstructing the video list and video item

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import styles from "./video_item.module.css";
 
-const VideoItem = (props) => (
-  <li className={styles.video}>
-    <img
-      className={styles.thumbnail}
-      src={props.video.snippet.thumbnails.medium.url}
-      alt="thumbnail"
-    />
-    <div className={styles.metadata}>
+const VideoItem = ({ video, video: { snippet } }) => (
+  <div className={styles.container}>
+    <li className={styles.video}>
       <img
-        className={styles.profileImg}
-        src={props.video.snippet.thumbnails.default.url}
-        alt="Profile image"
+        className={styles.thumbnail}
+        src={snippet.thumbnails.medium.url}
+        alt="thumbnail"
       />
-      <div>
-        <p className={styles.title}>{props.video.snippet.title}</p>
-        <p className={styles.channel}>{props.video.snippet.channelTitle}</p>
-        <p className={styles.viewCount}>
-          {props.video.statistics.viewCount} views
-        </p>
+      <div className={styles.metadata}>
+        <img
+          className={styles.profileImg}
+          src={snippet.thumbnails.default.url}
+          alt="Profile image"
+        />
+        <div>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+          <p className={styles.viewCount}>{video.statistics.viewCount} views</p>
+        </div>
       </div>
-    </div>
-  </li>
+    </li>
+  </div>
 );
 
 export default VideoItem;
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import VideoItem from "../video_item/video_item";
 import styles from "./video_list.module.css";
 
-const VideoList = (props) => (
+const VideoList = ({ videos }) => (
   <ul className={styles.videos}>
-    {props.videos.map((video) => (
+    {videos.map((video) => (
       <VideoItem key={video.id} video={video} />
     ))}
   </ul>
